5f7c9dd4db55f0689d365c439cdbbd3c3586c91e
dineshnallaiya <dinesh.nallaiya@cannyfore.com>
2026-04-13 10:20:35 +0530

    DEV-kp-03: Implemented the text embedding for search

--- server/app/api/services/file_handler_service.py
+++ server/app/api/services/file_handler_service.py
@@ -1,22 +1,91 @@
 import os
+from io import BytesIO
 from typing import List
 from fastapi import UploadFile
 from requests import Session
-from app.db.models import FileStorage
-from app.core.constants import FILE_DIRECTROY
+from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from docx import Document
+from sqlalchemy.exc import IntegrityError
+from app.core.constants import FILE_DIRECTROY, PDF, MODEL_NAME, DOCX
+from app.db.models import FileStorage, DocumentChunk
+
 
 os.makedirs(FILE_DIRECTROY, exist_ok=True)
 
+embedding_model = HuggingFaceEmbeddings(model_name=MODEL_NAME)
 
-def file_uploader(files: List[UploadFile], db: Session):
+
+async def file_uploader(files: List[UploadFile], db: Session):
     for file in files:
         file_path = os.path.join(FILE_DIRECTROY, file.filename)
-
+        content = await file.read()
         with open(file_path, "wb") as f:
             f.write(file.file.read())
+        texts = await text_extraction(content, file.filename)
+        chunks = await text_split(texts)
+        chunk_texts = [c["content"] for c in chunks]
+        embedded_text = get_embedding(chunk_texts)
+        insert_file = FileStorage(
+            file_name=file.filename,
+            file_path=file_path,
+        )
+        try:
+            for i, chunk in enumerate(chunks):
+                db.add(
+                    DocumentChunk(
+                        content=chunk["content"],
+                        embedding=embedded_text[i],
+                        extra_metadata={
+                            "file_name": file.filename,
+                            "chunk_id": i,
+                            "page": chunk["page"],
+                        },
+                    )
+                )
+            db.add(insert_file)
+            db.commit()
+            db.refresh(insert_file)
+        except IntegrityError:
+            db.rollback()
+            return {"message": "Duplicate data found"}
 
-        insert_file = FileStorage(file_name=file.filename, file_path=file_path)
-        db.add(insert_file)
-        db.commit()
-        db.refresh(insert_file)
         return {"message": "Successfully file uploaded."}
+
+
+async def text_extraction(content, filename):
+    documents = []
+    file_format = filename.split(".")[1]
+
+    if file_format == PDF:
+        reader = PdfReader(BytesIO(content))
+        for page_num, page in enumerate(reader.pages):
+            page_text = page.extract_text()
+            if page_text:
+                documents.append({"text": page_text, "page": page_num + 1})
+
+    elif file_format == DOCX:
+        doc = Document(BytesIO(content))
+        for i, para in enumerate(doc.paragraphs):
+            if para.text:
+                documents.append({"text": para.text, "page": i + 1})
+
+    return documents
+
+
+async def text_split(documents):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+
+    final_chunks = []
+
+    for doc in documents:
+        chunks = splitter.split_text(doc["text"])
+        for chunk in chunks:
+            final_chunks.append({"content": chunk, "page": doc["page"]})
+
+    return final_chunks
+
+
+def get_embedding(chunks):
+    return embedding_model.embed_documents(chunks)
--- server/app/db/models.py
+++ server/app/db/models.py
@@ -1,5 +1,6 @@
 from app.db.database import Base
-from sqlalchemy import String, Column, Integer
+from sqlalchemy import String, Column, JSON, Integer, Text, UniqueConstraint
+from pgvector.sqlalchemy import Vector
 
 
 class FileStorage(Base):
@@ -7,3 +8,12 @@ class FileStorage(Base):
     id = Column(Integer, primary_key=True)
     file_name = Column(String, nullable=False)
     file_path = Column(String, nullable=False)
+
+
+class DocumentChunk(Base):
+    __tablename__ = "document_chunks"
+    id = Column(Integer, primary_key=True)
+    content = Column(Text)
+    embedding = Column(Vector(384))
+    extra_metadata = Column(JSON)
+    __table_args__ = (UniqueConstraint("content", name="unique_chunk"),)
