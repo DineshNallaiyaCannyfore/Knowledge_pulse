ef1f1ee332bae124b5d94439758c0bae4f21b33f
nallaiya <dinesh.nallaiya@cannyfore.com>
2026-05-01 11:05:43 +0530

    DEV-kp-06: base search layout changes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,15 @@
 import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import "primeicons/primeicons.css";
+import Aura from "@primeuix/themes/aura";
+import ToastService from "primevue/toastservice";
 import "./style.css";
 import App from "./App.vue";
-import Aura from "@primeuix/themes/aura";
 const app = createApp(App);
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
   },
 });
+app.use(ToastService);
 app.mount("#app");
